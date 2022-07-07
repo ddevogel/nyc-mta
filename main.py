23976ce856c5d2d337f5c2d0996990a3c73a2f2c
ddevogel <ddevogel@yahoo.com>
2022-07-06 19:04:31 -0500

    Add data

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-from email import header
 from urllib.request import Request, urlopen
 from google.transit import gtfs_realtime_pb2
 import os
 from update import Update
 
+# from csv_to_json import make_json
+# dir = 'data/csv'
+# for filename in os.listdir(dir):
+#     f = os.path.join(dir, filename)
+#     make_json(f, f.replace('csv', 'json'))
 
 feed = gtfs_realtime_pb2.FeedMessage()
 req = Request('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace')
 req.add_header('x-api-key', os.environ.get('MTA_KEY'))
 response = urlopen(req)
 feed.ParseFromString(response.read())
+next = list()
 for entity in feed.entity:
     if entity.HasField('trip_update'):
-        print(entity.trip_update.trip)
+        trip_update = entity.trip_update 
+        id = trip_update.trip.trip_id.split('_')[1]
+        if not(id in next):
+            # print(trip_update)
+            next.append(id)
 
-        for update in entity.trip_update.stop_time_update:
-            x = Update(update.stop_id, update.arrival.time, update.departure.time)
-            print(x)
+
+            for update in trip_update.stop_time_update:
+                x = Update(update.stop_id, update.arrival.time, update.departure.time)
+                print(f"{id} - {x}")
         # print(entity.trip_update)
         # print(entity)
--- update.py
+++ update.py
@@ -2,13 +2,13 @@ from datetime import datetime
 import json
 class Update: 
 
-    stops = json.load(open('stops.json'))
+    stops = json.load(open('data/json/stops.json'))
     stop: str
     arrival: str
     departure: str 
 
     def __init__(self, stop_id: str, arrival: int, departure: int):
-        self.stop = "UNKNOWN" if stop_id not in Update.stops else Update.stops[stop_id]['name']
+        self.stop = "UNKNOWN" if stop_id not in Update.stops else Update.stops[stop_id]['stop_name']
         self.arrival = datetime.fromtimestamp(arrival).strftime("%m/%d/%Y, %H:%M:%S")
         self.departure = datetime.fromtimestamp(departure).strftime("%m/%d/%Y, %H:%M:%S")
 
